0a76b08f74df03d1a51f97c29b0ca59990774768
Sumit639 <sumitkkumar639@gmail.com>
2024-06-29 10:28:21 +0530

    delete

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Header from "./_components/Header";
-import Sample from "./_components/sample";
+
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <Sample/>
+
     </>
   );
 }
