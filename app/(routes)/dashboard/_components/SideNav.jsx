679a3264c11ae803677cba34b340b536efa93fb4
Sumit639 <sumitkkumar639@gmail.com>
2024-06-29 16:48:50 +0530

    update

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { LayoutGrid,PiggyBank,ReceiptText,ShieldCheck } from "lucide-react";
+import { LayoutGrid, PiggyBank, ReceiptText, ShieldCheck } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 const SideNav = () => {
   const menuList = [
@@ -28,12 +28,16 @@ const SideNav = () => {
   return (
     <>
       <div className="h-screen p-5 border shadow-sm">
-        <Image src={"/logo.svg"} alt="logo" width={160} height={100} />
+        <div className=" flex items-center space-x-3">
+          <Image src={"/logo.svg"} alt="logo" width={50} height={50} />
+          <span>Finance tracker</span>
+        </div>
         <div className="mt-5">
           {menuList.map((menu, index) => (
             <h2
               className="flex gap-2 items-center text-gray-500  font-medium p-5 cursor-pointer rounded-md 
-          hover:text-blue-800 hover: bg-blue-100" key={menu.id}
+          hover:text-blue-800 hover: bg-blue-100"
+              key={menu.id}
             >
               <menu.icon />
               {menu.name}
