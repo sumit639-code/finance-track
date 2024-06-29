1d8d266a8c936edea74db6ed3f9121e2ddb691de
jigyansha <routjig09@gmail.com>
2024-06-29 16:09:39 +0530

    navbar

---
+++ app/(routes)/dashboard/_components/_compponents/SideNav.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Image from "next/image";
+import { LayoutGrid, PiggyBank,ReceiptText,sheildCheck } from 'lucide-react';
+import { UserButton } from '@clerk/nextjs';
+
+function SideNav() {
+  const menuList=[
+    {
+      id:1,
+      name:'Dashboard',
+      icon:LayoutGrid
+    },
+    {
+      id:2,
+      name:'budgets',
+      icon:PiggyBank
+    },{
+      id:3,
+      name:'Expenses',
+      icon:ReceiptText
+    },{
+      id:4,
+      name:'Upgrade',
+      icon:sheildCheck
+    }
+  ]
+  return (
+    <div className='h-screen p-5 border shadow-sm'>
+      <Image src={'/logo.svg'}
+      alt='logo'
+      width={160}
+      height={100}/>
+      <div className='mt-5'>
+        {menuList.map((menu,index)=>(
+          <h2 className='flex gap-2 items-center text-gray-500  font-medium p-5 cursor-pointer rounded-md 
+          hover:text-blue-800 hover: bg-blue-100'>
+            <menu.icon/>
+            {menu.name}
+          </h2>
+
+        ))}
+      </div>
+      <div className='fixed bottom-10 p-5 flex hap-2 items-center'>
+        <userButton/>
+        profile
+      </div>
+    </div>
+  )
+}
+
+export default SideNav
