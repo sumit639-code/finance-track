7eee1da625a74ba8d0d7cfe15f2059339dc1cd04
Sumit639 <sumitkkumar639@gmail.com>
2024-06-29 09:51:27 +0530

    testing 2

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,12 +4,13 @@ import { Button } from '@/components/ui/button'
 const Header = () => {
   return (
     <>
-        <div className='p-5'>
+        <div className='p-5 flex justify-between'>
             <Image
             src={'./logo.svg'}
             alt='logo'
             width={50}
             height={50}/>
+            Finance tracker
             <Button>Get Started</Button>
         </div>
     </>
