a30bf166d53faebb1c59b85e4b2c242eff4bd10a
Sumit639 <sumitkkumar639@gmail.com>
2024-06-29 16:46:03 +0530

    Side nav bar fixing

---
+++ app/(routes)/dashboard/layout.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import SideNav from './_components/SideNav'
+// import DashboardHeader from './dashboard/DashboardHeader'
+
+function DashboardLayout({children}) {
+  return (
+    <div>
+          <div className='fixed md:w-64 hidden md:block bg-red-100'>
+          <SideNav/>
+          </div>
+          <div className='md:ml-64 bg-green-200' >
+           {/* <DashboardHeader/> */}
+          {children}      
+          </div>
+      
+    </div>
+  )
+}
+
+export default DashboardLayout
