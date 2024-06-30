2c34f36f64c1c15dbdee1092f1688917445cc171
Sumit639 <sumitkkumar639@gmail.com>
2024-06-30 09:17:00 +0530

    new update

--- Utils/schema.jsx
+++ Utils/schema.jsx
@@ -1,8 +1,17 @@
-import {pgTable, serial, varchar} from "drizzle-orm/pg-core"
+import {integer, numeric, pgTable, serial, varchar} from "drizzle-orm/pg-core"
 export const Budgets = pgTable('bugets',{
     id:serial('id').primaryKey(),
     name:varchar('name').notNull(),
     amount:varchar('amount'),
     icon:varchar('icon'),
+    createdBy:varchar('createdBy').notNull()
+})
+
+
+export const Expenses = pgTable('expenses',{
+    id:serial('id').primaryKey(),
+    name:varchar('name').notNull(),
+    amount:varchar('amount'),
+    budgetId:integer('budgetId').references(()=>Budgets.id),
     createdBy:varchar('createdBy').notNull()
 })
