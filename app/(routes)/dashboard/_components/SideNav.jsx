7cfcf3bda50dea184b08be9a39e178a0112c8dd7
Sumit639 <sumitkkumar639@gmail.com>
2024-06-30 12:04:18 +0530

    sidebar fixed

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,34 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { LayoutGrid, PiggyBank, ReceiptText, ShieldCheck } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
 const SideNav = () => {
   const menuList = [
     {
       id: 1,
       name: "Dashboard",
       icon: LayoutGrid,
-      path:'./dashboard'
+      path:'/dashboard'
     },
     {
       id: 2,
       name: "budgets",
       icon: PiggyBank,
-      path:'./dashboard/budgets'
+      path:'/dashboard/budgets'
     },
     {
       id: 3,
       name: "Expenses",
       icon: ReceiptText,
-      path:'./dashboard/expenses'
+      path:'/dashboard/expenses'
     },
     {
       id: 4,
       name: "Upgrade",
       icon: ShieldCheck,
-      path:'./dashboard/upgrade'
+      path:'/dashboard/upgrade'
     },
   ];
+  const path = usePathname();
+  useEffect(()=>{
+    console.log(path);
+  },[path])
   return (
     <>
       <div className="h-screen p-5 border shadow-sm">
@@ -38,14 +45,15 @@ const SideNav = () => {
         </div>
         <div className="mt-5">
           {menuList.map((menu, index) => (
+            <Link href={menu.path}>
             <h2
-              className="flex gap-2 items-center text-gray-500  font-medium p-5 cursor-pointer rounded-md 
-          hover:text-blue-800 hover:bg-blue-100"
+              className={`flex gap-2 items-center text-gray-500  font-medium p-5 cursor-pointer rounded-md 
+          hover:text-blue-800 hover:bg-blue-100 ${path==menu.path&&'text-primary bg-blue-100'}`}
               key={menu.id}
             >
               <menu.icon />
               {menu.name}
-            </h2>
+            </h2></Link>
           ))}
         </div>
         <div className="fixed bottom-10 p-5 flex gap-2 items-center">
